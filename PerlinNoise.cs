bdb1b57e1eb319fce03fc0c2942d4422096c97ab
RyanMercier <57643496+RyanMercier@users.noreply.github.com>
2020-07-15 17:12:20 -0400

    Add files via upload

---
+++ PerlinNoise.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise
+{
+    private int width;
+    private int height;
+    private float scale;
+    private float offsetX = 0;
+    private float offsetY = 0;
+
+    public Texture2D GenerateTexture(int width, int height, float scale)
+    {
+        this.width = width;
+        this.height = height;
+        this.scale = scale;
+        offsetX = Random.Range(0, 99999f);
+        offsetY = Random.Range(0, 99999f);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        for (int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculateColor(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+        
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+}
---
+++ TerrainGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    private int width;
+    private int height;
+    private int radius;
+    private float scale;
+    private float surfaceLevel = 0.3f;
+
+    private PerlinNoise noise;
+    private Color32 surfaceColor;
+    private Color32 noiseColor;
+
+    [HideInInspector]
+    public Texture2D terrain;
+
+    void Start()
+    {
+        //Generate Planet Colors
+        surfaceColor = new Color32((byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), 255);
+        noiseColor = new Color32((byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), 255);
+
+        //Generate Terrain Texture
+        int planetSize = UnityEngine.Random.Range(3, 20);
+        transform.localScale = new UnityEngine.Vector3(planetSize, planetSize, 1);
+
+
+        width = planetSize * 4 + 1;
+        height = planetSize * 4 + 1;
+        radius = width / 2;
+
+        scale = UnityEngine.Random.Range(5, planetSize);
+
+        terrain = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        noise = new PerlinNoise();
+        Texture2D noiseMap = noise.GenerateTexture(width, height, scale);
+
+        for (int x = 0; x < noiseMap.width; x++)
+        {
+            for(int y = 0; y < noiseMap.height; y++)
+            {
+                if (UnityEngine.Vector2.Distance(new UnityEngine.Vector2(radius, radius), new UnityEngine.Vector2(x, y)) > radius)
+                {
+                    terrain.SetPixel(x, y, Color.clear);
+                }
+                
+                else if (noiseMap.GetPixel(x, y).r < surfaceLevel)
+                {
+                    terrain.SetPixel(x, y, surfaceColor);
+                }
+
+                else
+                {
+                    Gradient g = new Gradient();
+                    GradientColorKey[] gck = new GradientColorKey[2];
+                    GradientAlphaKey[] gak = new GradientAlphaKey[2];
+                    gck[0].color = noiseColor;
+                    gck[0].time = 1.0F;
+                    gck[1].color = surfaceColor;
+                    gck[1].time = -1.0F;
+                    gak[0].alpha = 1.0F;
+                    gak[0].time = 1.0F;
+                    gak[1].alpha = 1.0F;
+                    gak[1].time = 1.0F;
+                    g.SetKeys(gck, gak);
+
+                    terrain.SetPixel(x, y, g.Evaluate(noiseMap.GetPixel(x, y).r));
+                }
+            }
+        }
+
+        terrain.filterMode = FilterMode.Point;
+        terrain.Apply();
+        GetComponent<Renderer>().material.mainTexture = terrain;
+    }
+}
